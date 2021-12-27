06186ceef1af1245ace7637ef405e0d52969beff
Sebalinux <sebalinux@sebalinux.it>
2021-12-27 17:12:09 +0100

    Modified execution list creation

--- main.go
+++ main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"math/rand"
 	"os"
-  	"math"
 	"strconv"
 	"time"
 )
@@ -12,16 +12,16 @@ import (
 type uselessFunc func()
 
 var vtp = map[int]uselessFunc{
-	0: download,
-	1: mvs,
-	2: bootVax,
-	3: c64,
-	4: adminports,
-	5: fsck,
-	6: bootMsDos,
-	7: stateLinux,
-	8: qradarAutoupdate,
-	9: pfSenseMenu,
+	0:  download,
+	1:  mvs,
+	2:  bootVax,
+	3:  c64,
+	4:  adminports,
+	5:  fsck,
+	6:  bootMsDos,
+	7:  stateLinux,
+	8:  qradarAutoupdate,
+	9:  pfSenseMenu,
 	10: vm370,
 	11: ciscoboot,
 }
@@ -30,50 +30,43 @@ var clo = map[int]uselessFunc{
 	0: standard,
 }
 
-func find(slice []int, val int) (int, bool) {
-    for i, item := range slice {
-        if item == val {
-            return i, true
-        }
-    }
-    return -1, false
-}
-
 func main() {
 	args := os.Args[1:]
 	rand.Seed(time.Now().UnixNano())
-	calls := 0
+	calls := rand.Intn(len(vtp))
 	var err error
 	if len(args) > 0 {
+		calls = 0
 		calls, err = strconv.Atoi(args[0])
 		if err != nil {
 			fmt.Print("ok, you tried to pass the number of how many program execute...\n")
 			fmt.Printf("but what you type is not a number [%v]\n", args[0])
 			fmt.Printf("so you will have the default for now :)\n")
 		}
-  	}
-  	calls = int(math.Min(float64(calls), float64(len(vtp))))
+	}
+	calls = int(math.Min(float64(calls), float64(len(vtp))))
 	if calls == 0 {
-		calls = 1 + rand.Intn(len(vtp) - 1)
+		calls = 1
 	}
 	fmt.Printf("here we go!  [%v]\n\n\n\n\n", calls)
-	
+
 	// create execution list
-  	myList := []int{}
-	
-	for i := 0; i < calls; {
-		randomNumber := rand.Intn(len(vtp))
-		_, found := find(myList, randomNumber)
-		if !found {
-			myList = append(myList, randomNumber)
-			i++
-		}
+	myList := []int{}
+
+	for i := 0; i < len(vtp); i++ {
+		myList = append(myList, i)
 	}
+
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(len(myList), func(i, j int) { myList[i], myList[j] = myList[j], myList[i] })
+
+	myList = myList[:calls]
+
 	for i := 0; i < len(myList); i++ {
 		call := vtp[myList[i]]
 		call()
 	}
+
 	call := clo[rand.Intn(len(clo))]
 	call()
 }
-
